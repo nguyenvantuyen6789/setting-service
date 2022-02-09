74f0485357609014bb73a355833de0e8ebeecea5
tuyennv <v.tuyennv18@vinhome.vn>
2022-02-09 09:02:44 +0700

    Xong! Tạo Eureka Client

--- src/main/java/com/tuyennguyen/setting/service/SettingServiceApplication.java
+++ src/main/java/com/tuyennguyen/setting/service/SettingServiceApplication.java
@@ -2,8 +2,10 @@ package com.tuyennguyen.setting.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SettingServiceApplication {
 
     public static void main(String[] args) {
